14563e633390ef56279f9f07a3a4c661cb968588
Royall Spence <royall@royall.us>
2018-07-31 11:10:42 -0400

    Stop trying to make Yew work

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -5,7 +5,9 @@
 extern crate rocket;
 extern crate rocket_contrib;
 
+use rocket::response::NamedFile;
 use rocket_contrib::Template;
+use std::path::{Path, PathBuf};
 
 #[derive(Serialize)]
 struct TemplateContext {
@@ -18,9 +20,15 @@ fn index() -> Template {
     Template::render("index", &context)
 }
 
+
+#[get("/<file..>")]
+fn files(file: PathBuf) -> Option<NamedFile> {
+    NamedFile::open(Path::new("static/").join(file)).ok()
+}
+
 fn main() {
     rocket::ignite()
-        .mount("/", routes![index])
+        .mount("/", routes![index, files])
         .attach(rocket_contrib::Template::fairing())
         .launch();
 }
--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,1 +1,28 @@
-fn main() {}
+#[macro_use]
+extern crate stdweb;
+
+use stdweb::web::{document, HtmlElement, INode, Node};
+
+fn main() {
+    stdweb::initialize();
+
+    match document().body() {
+        Some(b) => write_document(b),
+        None => console!(log, "couldn't get body")
+    }
+
+//    stdweb::event_loop();
+}
+
+fn write_document (body: HtmlElement) {
+    body.append_child(&h1("Rust running on Platform.sh"));
+    body.append_child(&p("This text was inserted by Javascript compiled from Rust"));
+}
+
+fn h1(body: &str) -> Node {
+    Node::from_html(format!("<h1>{}</h1>", body).as_str()).unwrap()
+}
+
+fn p(body: &str) -> Node {
+    Node::from_html(format!("<p>{}</p>", body).as_str()).unwrap()
+}
